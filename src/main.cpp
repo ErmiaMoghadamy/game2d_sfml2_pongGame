e4e4393432ce2f2fb7d8fbf2a429ce1eb1843eb2
Ermia <ermia.codev@gmail.com>
2025-12-11 16:47:55 +0330

    initial sfml setup

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Pong Game");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) window.close();
+        }
+    }
+    return 0;
+}
