ede00f8fa3be8aaf5c7a559d4dc2b547e73f81ba
ErmiaMoghadamy <ermia1303@gmail.com>
2025-12-11 17:03:45 +0330

    mod main.cpp: ClearColor smoke test

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,9 @@ int main() {
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) window.close();
         }
+
+        window.clear(sf::Color::Green);
+        window.display();
     }
     return 0;
 }
